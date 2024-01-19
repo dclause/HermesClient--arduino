8f05c470285bf99fd1366b482629d51a7eecaa31
Dominique CLAUSE <dom.clause@gmail.com>
2024-01-19 13:28:25 +0100

    First draft at working client.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
 #include <Arduino.h>
 
 // Only include debug for platformIO.
-#ifdef PLATFORMIO
-#include "avr8-stub.h"
-#include "app_api.h"
-#endif
+// #ifdef PLATFORMIO
+// #include "avr8-stub.h"
+// #include "app_api.h"
+// #endif
 
 void setup()
 {
 
-// Only include debug for platformIO.
-#ifdef PLATFORMIO
-  debug_init();
-#endif
+// // Only include debug for platformIO.
+// #ifdef PLATFORMIO
+//   debug_init();
+// #endif
 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial1.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
+  // read from port 0, send to port 1:
+  if (Serial.available())
+  {
+    int inByte = Serial.read();
+    if (inByte == 100) {
+      Serial.write(21);
+      Serial1.println(String((uint8_t)inByte));
+    }
+  }
 }
